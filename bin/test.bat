d90f25a45b222d843c00368a9d4ef5cd94e0025d
PomanoB <romanov4400@gmail.com>
2012-02-19 11:06:43 +0400

    Linux amd64 binary && test.bat and test.sh

---
+++ bin/test.bat
@@ -0,0 +1,10 @@
+@echo off
+mkdir results
+
+for %%K IN (1,2,5,10) DO serelex_win32.exe -c "sample-data\concepts.csv" -d "sample-data\definitions.csv" -s "sample-data\stoplist.csv" -o "results\overlap_knn_%%K.csv" -S o -M 1 -K %%K
+
+for %%K IN (1,2,5,10) DO serelex_win32.exe -c "sample-data\concepts.csv" -d "sample-data\definitions.csv" -s "sample-data\stoplist.csv" -o "results\overlap_mutual-knn_%%K.csv" -S o -M 2 -K %%K
+
+for %%K IN (1,2,5,10) DO serelex_win32.exe -c "sample-data\concepts.csv" -d "sample-data\definitions.csv" -s "sample-data\stoplist.csv" -o "results\cos_knn_%%K.csv" -S c -M 1 -K %%K
+
+for %%K IN (1,2,5,10) DO serelex_win32.exe -c "sample-data\concepts.csv" -d "sample-data\definitions.csv" -s "sample-data\stoplist.csv" -o "results\cos_mutual-knn_%%K.csv" -S c -M 2 -K %%K
